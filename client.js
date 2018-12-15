876cd0533e423f5382f20254ed45d324c978c0a7
Maxwell Todd <mtmaxtodd@gmail.com>
2018-12-15 13:49:12 -0600

    added a test file. All base mode complete.

--- client.js
+++ client.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
     $('#submit').on('click', addEmployee)
+    $('#employeeList').on('click', '.delete', removeRow)
 });
-
-let employeeArray = [];
-let monthlyCost = 0;
-
+let monthlyTotal = 0;
 function addEmployee(){
-    let firstInput = $('#firstInput').val();
-    let lastInput = $('#lastInput').val();
-    let idInput = $('#idInput').val();
-    let titleInput = $('#titleInput').val();
-    let salaryInput = $('#salaryInput').val();
-    newEmployee(firstInput, lastInput, idInput, titleInput, salaryInput);
+    $('#employeeList').append('<li>' + 
+    $('#firstInput').val() + ' ' +$('#lastInput').val() + ' ' +
+    $('#idInput').val() + ' ' + $('#titleInput').val() + ' ' +
+    $('#salaryInput').val() + '<button class="delete">X</li>');
+
+    $('#viewCosts').empty();
+    $('#viewCosts').append('Total Monthly: ' + (monthlyTotal + 
+    ($('#salaryInput').val()/12)).toFixed());
+    addTotal();
+
     $('#firstInput').val('');
     $('#lastInput').val('');
     $('#idInput').val('');
@@ -19,36 +21,12 @@ function addEmployee(){
     $('#salaryInput').val('');
 }
 
-function monthlyCosts(){
-   for (employee of employeeArray){
-       monthlyCost + (employee.annualSalary/12);
-   }
-   $('#viewCosts').append(total);
+function addTotal(){
+    monthlyTotal = monthlyTotal + ($('#salaryInput').val() / 12);  
 }
 
-class Employee {
-    constructor(firstName, lastName, employeeID, jobTitle, annualSalary) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.employeeID = employeeID;
-        this.jobTitle = jobTitle;
-        this.annualSalary = annualSalary;
-    } //end constructor
-}
-
-function newEmployee(firstName, lastName, employeeID, jobTitle, annualSalary){
-    employeeArray.push(new Employee(firstName, lastName, employeeID,
-         jobTitle, annualSalary));
-         addToList();
-}
-
-function addToList() {
-    let employeeInfo = $('#employeeList');
-    employeeInfo.empty();
-    for (employee of employeeArray) {
-        employeeInfo.append('<li>' + employee.firstName + ' ' + employee.lastName + ' '
-        + employee.employeeID + ' ' + employee.jobTitle + ' ' + employee.annualSalary
-        + '</li>')
-        monthlyCosts(); 
-    }
+function removeRow(){
+    $(this).parent().remove();
+    monthlyTotal = monthlyTotal - (this.$('#salaryInput').val()/12);
+    $('#viewCosts').append('Total Monthly: ' + monthlyTotal);
 }
